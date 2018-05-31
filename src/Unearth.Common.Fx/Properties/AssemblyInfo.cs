514daea13917adac48707cd076e1345971af3622
Kelly B <1802795+kellybirr@users.noreply.github.com>
2018-05-31 09:23:29 -0700

    Release 1.7.5

--- src/Unearth.Common.Fx/Properties/AssemblyInfo.cs
+++ src/Unearth.Common.Fx/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Unearth.Grpc")]
+[assembly: AssemblyTitle("Unearth")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Unearth.Grpc")]
+[assembly: AssemblyProduct("Unearth")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5ec07026-9154-46d8-a401-213c29eb78f6")]
+[assembly: Guid("2f769958-8093-4d86-9614-885ae1dbdd59")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.2.1020")]
-[assembly: AssemblyFileVersion("1.7.2.1020")]
+[assembly: AssemblyVersion("1.7.5.1090")]
+[assembly: AssemblyFileVersion("1.7.5.1090")]
--- src/Unearth.Core.Fx/Properties/AssemblyInfo.cs
+++ src/Unearth.Core.Fx/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Unearth")]
+[assembly: AssemblyTitle("Unearth.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Unearth")]
+[assembly: AssemblyProduct("Unearth.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2f769958-8093-4d86-9614-885ae1dbdd59")]
+[assembly: Guid("2f769958-8093-4d86-9614-885ae1dbdd58")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.2.1020")]
-[assembly: AssemblyFileVersion("1.7.2.1020")]
+[assembly: AssemblyVersion("1.7.5.1090")]
+[assembly: AssemblyFileVersion("1.7.5.1090")]
